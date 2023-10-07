0378215feda482cd5d51fe887b1c4a2b3a580b9e
LeonardCP <A01705466@tec.mx>
2023-10-07 10:22:28 -0600

    Contador y limitador de FPS
    Agregué un contador de FPS tanto en el nivel uno como en el menu y puse un limite para que el juego solo vaya maximo a 60 FPS

--- Assets/Karting/Scripts/FramerateCounter.cs
+++ Assets/Karting/Scripts/FramerateCounter.cs
@@ -8,8 +8,14 @@ public class FramerateCounter : MonoBehaviour
     [Tooltip("The text field displaying the framerate")]
     public TextMeshProUGUI uiText;
 
+    private int FPS_limit = 60;
     float m_AccumulatedDeltaTime = 0f;
     int m_AccumulatedFrameCount = 0;
+
+    private void Start()
+    {
+        Application.targetFrameRate = FPS_limit;
+    }
 
     void Update()
     {
@@ -19,9 +25,9 @@ public class FramerateCounter : MonoBehaviour
         if (m_AccumulatedDeltaTime >= pollingTime)
         {
             int framerate = Mathf.RoundToInt((float)m_AccumulatedFrameCount / m_AccumulatedDeltaTime);
-            uiText.text = framerate.ToString();
+            uiText.text = framerate.ToString() + " FPS";
 
-            m_AccumulatedDeltaTime = 0f;
+            m_AccumulatedDeltaTime -= pollingTime;
             m_AccumulatedFrameCount = 0;
         }
     }
